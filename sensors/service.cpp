51527c9c62286399fff7b6e68c3aa74a1a90a612
HyperRamzey <HyperRamzey@users.noreply.github.com>
2026-09-10 23:54:18 +0300

    sensors: park instead of exit(255) on passthrough init failure

--- sensors/service.cpp
+++ sensors/service.cpp
@@ -1,31 +1,21 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #define LOG_TAG "android.hardware.sensors@1.0-service_32"
 
 #include <android/hardware/sensors/1.0/ISensors.h>
 #include <hidl/LegacySupport.h>
+#include <log/log.h>
 
 using android::hardware::sensors::V1_0::ISensors;
-using android::hardware::defaultPassthroughServiceImplementation;
 
 int main() {
-    /* Sensors framework service needs at least two threads.
-     * One thread blocks on a "poll"
-     * The second thread is needed for all other HAL methods.
-     */
-    return defaultPassthroughServiceImplementation<ISensors>(2);
+    /* Framework needs the sensors HAL to stay alive: SystemServer blocks on
+     * waitForSensorService() and A16 init escalates UPDATABLE_CRASHING reboots
+     * when this process exits repeatedly (observed exit-255 every ~7s).
+     * defaultPassthroughServiceImplementation exits -1 when the underlying
+     * hw module init fails - log the reason and park instead of exiting. */
+    int ret = android::hardware::defaultPassthroughServiceImplementation<ISensors>(2);
+    ALOGE("sensors passthrough service returned %d - parking (no exit)", ret);
+
+    /* Keep the process alive so init does not crash-loop the boot; the HAL
+     * interface is simply absent and frameworks handle that gracefully. */
+    for (;;) pause();
 }
